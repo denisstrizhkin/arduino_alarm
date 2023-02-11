f3371ab7219ddc73ec1e63ec4b9fa0b035e9efb0
Denis Strizhkin <strdenis02@gmail.com>
2023-02-11 20:35:17 +0300

    lcd library test

--- arduino_alarm.ino
+++ arduino_alarm.ino
@@ -1,6 +1,16 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <LiquidCrystal_I2C.h>
 
+LiquidCrystal_I2C lcd(0x27,16,2); // set the LCD address to 0x27 for a 16 chars and 2 line display
+
+void setup()
+{
+  lcd.init(); // initialize the lcd 
+  // Print a message to the LCD.
+  lcd.backlight();
+  lcd.setCursor(5,0);
+  lcd.print("Kolya Pidr!");
+  //lcd.setCursor(1,1);
+  //lcd.print("Ywrobot Arduino!");
 }
 
 void loop() {

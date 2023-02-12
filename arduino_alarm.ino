1fa121431da66e7d7ff8035bcc79eb16736bbf91
Denis Strizhkin <strdenis02@gmail.com>
2023-02-12 22:56:33 +0300

    finished basic functionality

--- arduino_alarm.ino
+++ arduino_alarm.ino
@@ -7,11 +7,12 @@ const int RTC_SCLK = 5;
 const int RTC_CE = 2;
 
 const int LED_PIN = 8;
-const int BTN_1_PIN = 11;
-const int BTN_2_PIN = 12;
+const int BTN_1_PIN = A1;
+const int BTN_3_PIN = A2;
+const int BTN_2_PIN = A3;
 const int POT_PIN = A0;
 
-const int DELAY_PERIOD = 1000 / 5;
+const int DELAY_PERIOD = 1000 / 15;
 
 const int MINUTES_FRACTION = 1024 / 60;
 const int HOURS_FRACTION = 1024 / 24;
@@ -25,35 +26,44 @@ unsigned long previous_time;
 int time_delta;
 int delay_time;
 
+int led_value = LOW;
 int pot_value;
 int btn_1_state;
 int btn_2_state;
+int btn_3_state;
+
+bool is_btn_1= false;
+bool is_btn_1_available = true;
+bool is_btn_2 = false;
+bool is_btn_2_available = true;
+bool is_btn_3 = false;
+bool is_btn_3_available = true;
 
 int minutes;
 int hours;
 
-int alarm_minutes;
-int alarm_hours;
+int alarm_minutes = -1;
+int alarm_hours = -1;
 
 enum State { regular, set_time, alarm };
+enum SetTimeType { hour, minute };
 
-State status;
+State status = regular;
+SetTimeType set_time_type = hour;
 
 void setup() {
   // setup led
   pinMode(LED_PIN, OUTPUT);
-  digitalWrite(LED_PIN, HIGH);
 
-  // setup buttons
-  pinMode(BTN_1_PIN, INPUT);
-  pinMode(BTN_2_PIN, INPUT);
+  // setup buttons for digital input
+  //pinMode(BTN_1_PIN, INPUT);
+  //pinMode(BTN_2_PIN, INPUT);
 
   // setup lcd
   lcd.init();
   lcd.backlight();
 
-  // variables initialization
-  status = regular;
+  // set initial time
   previous_time = millis();
 
   // rtc setup
@@ -95,33 +105,23 @@ void setup() {
 }
 
 void loop() {
+  digitalWrite(LED_PIN, led_value);
   lcd.setCursor(0, 0);
   pot_value = analogRead(POT_PIN);
-  btn_1_state = digitalRead(BTN_1_PIN);
-  btn_2_state = digitalRead(BTN_2_PIN);
+  btn_1_state = analogRead(BTN_1_PIN);
+  btn_2_state = analogRead(BTN_2_PIN);
+  btn_3_state = analogRead(BTN_3_PIN);
 
-  minutes = pot_value / MINUTES_FRACTION;
-  minutes = (minutes == 60 ? minutes - 1: minutes);
+  if (status == set_time || status == alarm) {
+    if (set_time_type == minute) {
+      minutes = pot_value / MINUTES_FRACTION;
+      minutes = (minutes == 60 ? minutes - 1: minutes);
+    }
 
-  hours = pot_value / HOURS_FRACTION;
-  hours = (hours == 24 ? hours - 1 : hours);
-
-  if (btn_1_state == HIGH) {
-    Serial.println("btn_1 - HIGH" + String(btn_1_state));
-  } else {
-    Serial.println("btn_1 - LOW" + String(btn_1_state));
-  }
-  
-  switch (status) {
-    case regular:
-        lcd.print("regular mode");
-      break;
-    case set_time:
-        lcd.print("set_time mode");
-      break;
-    case alarm:
-        lcd.print("alarm mode");
-      break;
+    if (set_time_type == hour) {
+      hours = pot_value / HOURS_FRACTION;
+      hours = (hours == 24 ? hours - 1 : hours);
+    }
   }
 
   RtcDateTime now = Rtc.GetDateTime();
@@ -130,6 +130,66 @@ void loop() {
     //   1) the battery on the device is low or even missing and the power line was disconnected
     Serial.println("RTC lost confidence in the DateTime!");
   }
+
+  if (btn_1_state == 0) {
+    if (is_btn_1_available) {
+      is_btn_1_available = false;
+      Serial.println("btn 1 pressed");
+      if (status == regular) {
+        status = set_time;
+      } else if (status == set_time) {
+        status = regular;
+        Rtc.SetDateTime(RtcDateTime(2002,1,1,hours,minutes,now.Second()));
+        set_time_type = hour;
+      }
+    }
+  } else {
+    is_btn_1_available = true;
+  }
+
+  if (btn_3_state == 0) {
+    if (is_btn_3_available) {
+      is_btn_3_available = false;
+      Serial.println("btn 3 pressed");
+      if (status == regular) {
+        status = alarm;
+      } else if (status == alarm) {
+        status = regular;
+        alarm_hours = hours;
+        alarm_minutes = minutes;
+        set_time_type = hour;
+      }
+    }
+  } else {
+    is_btn_3_available = true;
+  }
+
+  if (btn_2_state == 0) {
+    if (is_btn_2_available && (status == set_time || status == alarm)) {
+      is_btn_2 = true;
+      is_btn_2_available = false;
+      set_time_type = (set_time_type == hour ? minute : hour);
+      Serial.println("btn 2 pressed");
+    }
+  } else {
+    is_btn_2_available = true;
+  }
+  
+  switch (status) {
+    case regular:
+        lcd.print("regular mode  ");
+      break;
+    case set_time:
+        lcd.print("set_time mode");
+      break;
+    case alarm:
+        lcd.print("alarm mode    ");
+      break;
+  }
+
+  if (now.Hour() == alarm_hours && now.Minute() == alarm_minutes) {
+    led_value = HIGH;
+  }
   
   lcd.setCursor(0, 1);
   lcd.print(padTime(now.Hour()));
@@ -137,6 +197,17 @@ void loop() {
   lcd.print(padTime(now.Minute()));
   lcd.setCursor(6, 1);
   lcd.print(padTime(now.Second()));
+
+  
+  if ((status == set_time || status == alarm)) {
+    lcd.setCursor(10, 1);
+    lcd.print(padTime(hours));
+    lcd.setCursor(13, 1);
+    lcd.print(padTime(minutes));
+  } else {
+    lcd.setCursor(10,1);
+    lcd.print("      ");
+  }
 
   // delay handling
   time_delta = millis() - previous_time;

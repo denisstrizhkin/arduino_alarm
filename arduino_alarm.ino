4514ebb3a7bed6d081253af72bac833643b8cd6d
Denis Strizhkin <strdenis02@gmail.com>
2023-02-12 17:01:05 +0300

    State enum

--- arduino_alarm.ino
+++ arduino_alarm.ino
@@ -8,17 +8,23 @@ const int POT_PIN = A0;
 const int DELAY_PERIOD = 1000 / 5;
 
 const int MINUTES_FRACTION = 1024 / 60;
-const int HOURS_FRACTION = 1024 / 12;
+const int HOURS_FRACTION = 1024 / 24;
 
 unsigned long previous_time;
 int time_delta;
 int delay_time;
 
-int cursorPos;
 int pot_value;
 
 int minutes;
 int hours;
+
+int alarm_minutes;
+int alarm_hours;
+
+enum State { regular, set_time, alarm };
+
+State status;
 
 void setup() {
   // setup led
@@ -28,33 +34,45 @@ void setup() {
   // setup lcd
   lcd.init();
   lcd.backlight();
-  cursorPos = 0;
 
+  status = regular;
   previous_time = millis();
 }
 
 void loop() {
-  /*
-  lcd.clear();
-  lcd.setCursor(cursorPos, 0);
-  lcd.print("Test!");
-  cursorPos = (cursorPos == 15 ? 0 : cursorPos + 1);
-  */
+  lcd.setCursor(0, 0);
   pot_value = analogRead(POT_PIN);
 
   minutes = pot_value / MINUTES_FRACTION;
-  minutes = (minutes == 60 ? minutes: minutes + 1);
+  minutes = (minutes == 60 ? minutes - 1: minutes);
 
   hours = pot_value / HOURS_FRACTION;
-  hours = (hours == 12 ? hours : hours + 1);
-
-  lcd.clear();
+  hours = (hours == 24 ? hours - 1 : hours);
+  
+  switch (status) {
+    case regular:
+        lcd.print("regular mode");
+      break;
+    case set_time:
+        lcd.print("set_time mode");
+      break;
+    case alarm:
+        lcd.print("alarm mode");
+      break;
+  }
+  
   lcd.setCursor(0, 1);
-  lcd.print(hours);
+  lcd.print(padTime(hours));
+  lcd.setCursor(3, 1);
+  lcd.print(padTime(minutes));
 
   // delay handling
   time_delta = millis() - previous_time;
   delay_time = (time_delta < DELAY_PERIOD ? DELAY_PERIOD - time_delta : 0);
   delay(delay_time);
   previous_time = millis();
+}
+
+String padTime(int time) {
+  return time > 9 ? String(time) : "0" + String(time);
 }
